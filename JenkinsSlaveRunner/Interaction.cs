03e6c80716fa5c9fc2d6a286d6d305aa0bfdc7a1
stuartjsmith <stuartjsmith@gmail.com>
2015-09-14 15:26:50 +0100

    Added missing comments

--- JenkinsSlaveRunner/Interaction.cs
+++ JenkinsSlaveRunner/Interaction.cs
@@ -1,9 +1,25 @@
-﻿using System.Windows;
+﻿// <copyright file="Interaction.cs" company="MyCompany.com">
+// Copyright (c) 2015 MyCompany.com. All rights reserved.
+// </copyright>
+// <author>Stuart. Smith</author>
+// <date>14/09/2015</date>
+// <summary>Implements the interaction class</summary>
+using System.Windows;
 
 namespace JenkinsSlaveRunner
 {
+    /// <summary>
+    /// An interaction.
+    /// </summary>
     internal static class Interaction
     {
+        /// <summary>
+        /// Confirm stop jenkins slave.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>
+        /// true if it succeeds, false if it fails.
+        /// </returns>
         internal static bool ConfirmStopJenkinsSlave(string message)
         {
             // Ask the user if they want to allow the session to end 
